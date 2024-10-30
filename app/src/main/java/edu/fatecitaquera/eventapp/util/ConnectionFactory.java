91d5c6e7e695be9553c79e570b0928e8a5a6b76e
vitormneris <joaomoreiraneris0@gmail.com>
2024-10-30 08:19:38 -0300

    Mundaça da cor principal - suavização

--- app/src/main/java/edu/fatecitaquera/eventapp/util/ConnectionFactory.java
+++ app/src/main/java/edu/fatecitaquera/eventapp/util/ConnectionFactory.java
@@ -1,6 +1,6 @@
 package edu.fatecitaquera.eventapp.util;
 
 public class ConnectionFactory  {
-    public static String serverIP = "18.188.197.165";
+    public static String serverIP = "52.15.228.213";
 }
 
